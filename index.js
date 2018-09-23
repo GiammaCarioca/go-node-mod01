6d88ed324edcb6eb03c3f9060ba1eca2a96e45b0
Thiago Giammattey <giammattey@gmail.com>
2018-09-23 18:48:43 -0300

    adicionando editorconfig, eslint e gitignore

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
-  express: app
+  express: app,
 });
 
 // configurando extensão dos arquivos
-app.set("view engine", "njk");
+app.set('view engine', 'njk');
 
 // configurando o caminho das views
-app.set("views", path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.post("/auth", (req, res) => {
+app.post('/auth', (req, res) => {
   const { username, password } = req.body;
   res.send(`Você logou com usuário ${username} e senha ${password}`);
 });
