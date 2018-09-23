23834501ea8a9fed53dd6b4f67bfb321d16ec1a9
Thiago Giammattey <giammattey@gmail.com>
2018-09-23 17:08:55 -0300

    usando app.use para executar middleware em todas as rotas que incluam a rota user

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
 
-const userMiddleware = (req, res, next) => {
-  console.log("Acessou!");
-  next();
-};
-
 app.get("/teste", (req, res) => {
   res.send("Hello Rocketseat");
 });
 
-app.get("/user", userMiddleware, (req, res) => {
+app.use("/user", (req, res, next) => {
+  console.log("Acessou!");
+  next();
+});
+
+app.get("/user", (req, res) => {
   res.send("Usuário: Giamma");
+});
+
+app.get("/user/name", (req, res) => {
+  res.send("Usuário: Thiago Giammattey");
 });
 
 app.listen(3000);
